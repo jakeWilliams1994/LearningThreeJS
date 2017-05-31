e4fb8047b230afbcc8627620a209fdb80d7805df
DESKTOP-LBSS1Q2\jakeJoestar <jakefwilliams1994@gmail.com>
2017-05-31 17:37:13 +0100

    LearningThreeJS: removed one of the copy drawAxis functions because it was just duplicate code. Added a new funtion that returns a sphere mesh object. Particles example now places 100 spheres of the same size with random positions between 0-100 on the x/y/z axis.

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,5 @@
-function drawAxis(xLength, yLength, zLength, scene) {
+// requires a valid scene to exist when called 
+function drawAxis(xLength, yLength, zLength) {
 	var lineMaterial = new THREE.LineBasicMaterial();
 	var geometry = new THREE.Geometry();
 	geometry.vertices.push(
@@ -12,16 +13,8 @@ function drawAxis(xLength, yLength, zLength, scene) {
 	scene.add(line); 
 }
 
-function drawAxis(axisLength, scene) {
-	var lineMaterial = new THREE.LineBasicMaterial();
-	var geometry = new THREE.Geometry();
-	geometry.vertices.push(
-		new THREE.Vector3(axisLength,0,0),
-		new THREE.Vector3(0,0,0),
-		new THREE.Vector3(0,axisLength,0),
-		new THREE.Vector3(0,0,0),
-		new THREE.Vector3(0,0,axisLength)
-	);
-	var line = new THREE.Line(geometry,lineMaterial);
-	scene.add(line); 
+function createBasicSphere(radius, widthSeg, HeightSeg) {
+	var geometry = new THREE.SphereGeometry(radius,widthSeg,HeightSeg);
+	var material = new THREE.MeshBasicMaterial();
+	return new THREE.Mesh(geometry,material);
 }
