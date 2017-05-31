16702a20faf7d120bed7dc1d5892fc8a36d5b5bc
DESKTOP-LBSS1Q2\jakeJoestar <jakefwilliams1994@gmail.com>
2017-05-31 22:54:14 +0100

    LearningThreeJS: Overview, particles now bounce between the axis min and max value (hardcoded to 0-100). Utils.js, added a few more functions to create an array of particle objects, a funciton to create a particle object and a function to make getting a random int between a min and max value more readable. Particles.html, we now store particles in an array rather than a group object. We also animate the particles to just bounce between 0-100 with the xyz speeds of 1.

--- js/utils.js
+++ js/utils.js
@@ -18,3 +18,21 @@ function createBasicSphere(radius, widthSeg, HeightSeg) {
 	var material = new THREE.MeshBasicMaterial();
 	return new THREE.Mesh(geometry,material);
 }
+
+function createParticle() {
+	return {particle:createBasicSphere(1,1,1),
+			xSpeed:1,
+			ySpeed:1,
+			zSpeed:1};
+}
+
+function createParticlesArray(arraySize) {
+	var particles = [];
+	for (var i = 0; i < arraySize; i++)
+		particles.push(createParticle());
+	return particles;
+}
+
+function randomInt(min, max) {
+	return Math.floor((Math.random() * max) + min)
+}
